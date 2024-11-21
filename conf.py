58a5f02fa0744f028e72d7fce56ea425ddad420f
Chandra Earl <sunray1@bellsouth.net>
2024-11-21 18:44:15 -0500

    Renamed header home

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "HtH Organizers"  # (translatable)
-BLOG_TITLE = "Hack the Hackathon Conference Series"  # (translatable)
+BLOG_TITLE = "Hack the Hackathon"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "http://hackthehackathon.org/"
