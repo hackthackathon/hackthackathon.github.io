e71d4dfae5b297985d8d0b66b0d48ce85f5b5ce0
Erik Tollerud <erik.tollerud@gmail.com>
2024-11-21 11:15:55 -0800

    update to base index

--- conf.py
+++ conf.py
@@ -236,16 +236,16 @@ THEME_CONFIG = {
 #     )
 
 POSTS = (
-    ("posts/*.rst", "posts", "post.tmpl"),
-    ("posts/*.md", "posts", "post.tmpl"),
-    ("posts/*.txt", "posts", "post.tmpl"),
-    ("posts/*.html", "posts", "post.tmpl"),
+    #("posts/*.rst", "news", "post.tmpl"),
+    ("posts/*.md", "news", "post.tmpl"),
+    ("posts/*.txt", "news", "post.tmpl"),
+    ("posts/*.html", "news", "post.tmpl"),
 )
 PAGES = (
-    ("pages/*.rst", "pages", "page.tmpl"),
-    ("pages/*.md", "pages", "page.tmpl"),
-    ("pages/*.txt", "pages", "page.tmpl"),
-    ("pages/*.html", "pages", "page.tmpl"),
+    #("pages/*.rst", "", "page.tmpl"),
+    ("pages/*.md", "", "page.tmpl"),
+    ("pages/*.txt", "", "page.tmpl"),
+    ("pages/*.html", "", "page.tmpl"),
 )
 
 
