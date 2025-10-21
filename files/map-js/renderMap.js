ba7935663c6afcd2d4caaa1758dda40820b417ed
Alexander Nolte <alexander.nolte@ut.ee>
2025-10-21 15:57:07 +0200

    made map more pretty

--- files/map-js/renderMap.js
+++ files/map-js/renderMap.js
@@ -1,17 +1,49 @@
 function renderMap() {
   const map = L.map('map').setView([20, 0], 2);
+  map.createPane('topDots');
+  map.getPane('topDots').style.zIndex = 650;
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
-    attribution: '© OpenStreetMap contributors'
+  map.whenReady(() => {
+    const bounds = map.getBounds();
+    map.setMaxBounds(bounds);            // prevent panning outside the initial area
+    map.setMinZoom(map.getZoom());       // prevent zooming out
+    map.options.maxBoundsViscosity = 1.0; // smooth bounce-back effect
+  });
+
+  L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    attribution: '',
+    maxZoom: 18
   }).addTo(map);
 
-  const markers = L.markerClusterGroup();
+  fetch("https://raw.githubusercontent.com/datasets/geo-boundaries-world-110m/master/countries.geojson")
+  .then(response => response.json())
+  .then(geojson => {
+    L.geoJSON(geojson, {
+      style: {
+        color: "#d3d3d3",
+        weight: 1.5,
+        opacity: 1,
+        fillColor: "#000000",
+        fillOpacity: 0.7
+      }
+    }).addTo(map);
+  });
+
+  const markers = L.markerClusterGroup({
+    showCoverageOnHover: false
+  });
 
   locations.forEach(loc => {
-    const marker = L.marker([loc.lat, loc.lng])
-      .bindPopup(`<b>${loc.name}</b>`);
-    markers.addLayer(marker);
+    const circle = L.circleMarker([loc.lat, loc.lng], {
+      radius: 6,
+      color: '#ff0000',
+      fillColor: 'transparent',
+      fillOpacity: 0,
+      weight: 3,
+      pane: 'topDots'
+    }).bindPopup(`<b>${loc.name}</b>`);
+
+    markers.addLayer(circle);
   });
 
   map.addLayer(markers);
